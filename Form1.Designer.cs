c8c291958a8533b95c502dd294ca148629a373d0
mestepanova <anna-smir-no-va@mail.ru>
2024-03-07 03:30:34 +0700

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab2_stepanova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPredict = new System.Windows.Forms.Button();
+            this.editRate = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.editRateDollar = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editRateDollar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.editRateDollar);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnPredict);
+            this.panel1.Controls.Add(this.editRate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(190, 441);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnPredict
+            // 
+            this.btnPredict.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPredict.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPredict.Location = new System.Drawing.Point(8, 133);
+            this.btnPredict.Name = "btnPredict";
+            this.btnPredict.Size = new System.Drawing.Size(174, 47);
+            this.btnPredict.TabIndex = 4;
+            this.btnPredict.Text = "Спрогнозировать (стоп/старт)";
+            this.btnPredict.UseVisualStyleBackColor = false;
+            this.btnPredict.Click += new System.EventHandler(this.btnPredict_Click);
+            // 
+            // editRate
+            // 
+            this.editRate.DecimalPlaces = 2;
+            this.editRate.Location = new System.Drawing.Point(102, 34);
+            this.editRate.Name = "editRate";
+            this.editRate.Size = new System.Drawing.Size(80, 20);
+            this.editRate.TabIndex = 2;
+            this.editRate.Value = new decimal(new int[] {
+            7974,
+            0,
+            0,
+            131072});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текущий курс евро: ";
+            // 
+            // chart1
+            // 
+            chartArea4.AxisX.Minimum = 0D;
+            chartArea4.AxisY.Minimum = 60D;
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(193, 6);
+            this.chart1.Name = "chart1";
+            series7.BorderWidth = 2;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.IsValueShownAsLabel = true;
+            series7.LabelBorderWidth = 6;
+            series7.LabelForeColor = System.Drawing.Color.DarkCyan;
+            series7.LabelFormat = "C";
+            series7.Legend = "Legend1";
+            series7.Name = "Euro";
+            series7.YValuesPerPoint = 4;
+            series8.BorderWidth = 2;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.IsValueShownAsLabel = true;
+            series8.LabelBorderWidth = 6;
+            series8.LabelForeColor = System.Drawing.Color.Purple;
+            series8.LabelFormat = "C";
+            series8.Legend = "Legend1";
+            series8.Name = "Dollar";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(600, 440);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(7, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Текущий курс доллара: ";
+            // 
+            // editRateDollar
+            // 
+            this.editRateDollar.DecimalPlaces = 2;
+            this.editRateDollar.Location = new System.Drawing.Point(102, 96);
+            this.editRateDollar.Name = "editRateDollar";
+            this.editRateDollar.Size = new System.Drawing.Size(80, 20);
+            this.editRateDollar.TabIndex = 6;
+            this.editRateDollar.Value = new decimal(new int[] {
+            7533,
+            0,
+            0,
+            131072});
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editRateDollar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown editRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnPredict;
+        private System.Windows.Forms.NumericUpDown editRateDollar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
